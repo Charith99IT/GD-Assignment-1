6fbd86bc4262ef06071148c535b7239457e92f51
Charith99IT <charithmadusankha99@gmail.com>
2022-03-28 18:32:22 +0530

    bug fix

--- GD Assignment 1/Assets/Scripts/Health/Health.cs
+++ GD Assignment 1/Assets/Scripts/Health/Health.cs
@@ -66,4 +66,10 @@ public class Health : MonoBehaviour
         Physics2D.IgnoreLayerCollision(10, 11, false);
         invulnerable = false;
     }
+
+    private void Deactivate()
+    {
+        gameObject.SetActive(false);
+    
+    }
 }
